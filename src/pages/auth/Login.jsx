f5e58585623ae9aaa43c76042f8e38a9f2d9aa3e
akshay0077 <kherakshay007@gmail.com>
2024-01-22 11:28:06 +0530

    [Added]:Auth Completed

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Auth.css";
+import { Link } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../store/authSlice.jsx";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Login
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.trim().length !== 0;
+  };
+
+  const validations = async () => {
+    const { email, password } = loginData;
+
+    if (!validateEmail(email)) {
+      toast("Please enter a valid email address", {
+        duration: 2000,
+        icon: "❌",
+      });
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      toast("Password is required", {
+        duration: 2000,
+        icon: "❌",
+      });
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        loginData
+      );
+      dispatch(loginUser(response.data));
+      console.log(response.data);
+    } catch (error) {
+      console.error("Login failed:", error);
+      toast("Login failed. Please check your credentials and try again.", {
+        duration: 3000,
+        icon: "❌",
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="form-container ">
+      <form>
+        <h4 className="title">LOGIN FORM</h4>
+
+        <div className="mb-3">
+          <input
+            type="email"
+            autoFocus
+            name="email"
+            value={loginData.email}
+            onChange={handleChange}
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Email "
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            name="password"
+            value={loginData.password}
+            onChange={handleChange}
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <button type="button" className="btn btn-primary" onClick={validations}>
+          LOGIN
+        </button>
+        <Toaster />
+        <br />
+        <span className="">Don't have an account?</span>
+        <Link to="/">Signup</Link>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,9 +1,239 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+import "./Auth.css";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../store/authSlice.jsx";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Register
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    conformPassword: "",
+  });
+
+  const navigate = useNavigate();
+
+  const validateFirstName = (firstName) => {
+    return firstName.trim().length !== 0;
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/;
+    return regex.test(password);
+  };
+
+  const displayErrorToast = (error) => {
+    toast.error(error);
+  };
+
+  const handleInvalidFirstName = () => {
+    const errorMessage = "Please enter a valid first name";
+    setFormState((prevState) => ({
+      ...prevState,
+      firstNameError: errorMessage,
+    }));
+    displayErrorToast(errorMessage);
+  };
+
+  const handleInvalidEmail = () => {
+    const errorMessage = "Please enter a valid email address";
+    setFormState((prevState) => ({
+      ...prevState,
+      emailError: errorMessage,
+    }));
+    displayErrorToast(errorMessage);
+  };
+
+  const handlePasswordMismatch = () => {
+    setFormState((prevState) => ({
+      ...prevState,
+      confirmPasswordError: "Passwords do not match",
+    }));
+    displayErrorToast("Passwords do not match");
+  };
+
+  const handleInvalidPassword = () => {
+    const errorMessage =
+      "Password must be at least 6 characters, contain one capital letter, one digit, and one special character";
+    setFormState((prevState) => ({
+      ...prevState,
+      passwordError: errorMessage,
+    }));
+    displayErrorToast(errorMessage);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { password, conformPassword, firstName, email } = formState;
+
+    if (!validateFirstName(firstName)) {
+      handleInvalidFirstName();
+      return;
+    } else {
+      setFormState((prevState) => ({
+        ...prevState,
+        firstNameError: "",
+      }));
+    }
+
+    if (!validateEmail(email)) {
+      handleInvalidEmail();
+      return;
+    } else {
+      setFormState((prevState) => ({
+        ...prevState,
+        emailError: "",
+      }));
+    }
+
+    if (password !== conformPassword) {
+      handlePasswordMismatch();
+      return;
+    } else {
+      setFormState((prevState) => ({
+        ...prevState,
+        confirmPasswordError: "",
+      }));
+    }
+
+    if (!validatePassword(password)) {
+      handleInvalidPassword();
+      return;
+    } else {
+      setFormState((prevState) => ({
+        ...prevState,
+        passwordError: "",
+      }));
+    }
+
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/register`,
+        formState
+      );
+
+      if (res && res.data.success) {
+        dispatch(loginUser(res.data));
+        toast.success(res.data && res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+
+    setFormState((prevState) => ({
+      ...prevState,
+      passwordError: "",
+      confirmPasswordError: "",
+      firstNameError: "",
+      emailError: "",
+    }));
+  };
+
+  return (
+    <>
+      <div className="form-container">
+        <form>
+          <h4 className="title">REGISTER FORM</h4>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="firstName"
+              value={formState.firstName}
+              onChange={handleChange}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="First Name"
+              required
+              autoFocus
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="lastName"
+              value={formState.lastName}
+              onChange={handleChange}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Last Name"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="example@gmail.com"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              name="conformPassword"
+              value={formState.conformPassword}
+              onChange={handleChange}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Conform Password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            REGISTER
+          </button>
+          <Toaster />
+          <br />
+          <span className="">Already have account?</span>
+          <Link to="/login">Login</Link>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
