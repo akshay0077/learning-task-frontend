9176bebda3a13c7d59233bc6591c2dc0924b8dce
Kher_Akshay7 <64765656+akshay0077@users.noreply.github.com>
2024-02-07 14:19:45 +0530

    Authentication  (#2)
    * [Added]:Auth Completed
    
    * [Resolved]: Auth migrate with PR Resolved
    
    * [Fixed]: Resolved PR in Authentication
    
    * [Resolved]: modify the login page
    
    * [Resolved]: fix the PR issue
    
    * [Resolved]: modify PR changes
    
    * [Resolved]: removed setTimeout

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+
+import "./Auth.css";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
-export default Login
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.trim().length !== 0;
+  };
+
+  const validations = async () => {
+    setIsSubmitting(true);
+    setDisabled(true);
+
+    const { email, password } = loginData;
+
+    if (!validateEmail(email)) {
+      toast("Please enter a valid email address", {
+        duration: 2000,
+        icon: "❌",
+      });
+    } else if (!validatePassword(password)) {
+      toast("Password is required", {
+        duration: 2000,
+        icon: "❌",
+      });
+    } else {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_API}/api/v1/auth/login`,
+          loginData
+        );
+
+        localStorage.setItem("user", JSON.stringify(response.data.sendPayload));
+        localStorage.setItem("token", response.data.token);
+
+        navigate("/");
+      } catch (error) {
+        toast("Login failed. Please check your credentials and try again.", {
+          duration: 3000,
+          icon: "❌",
+        });
+      }
+    }
+
+    setIsSubmitting(false);
+    setDisabled(false);
+  };
+
+  const handleChange = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="form-container ">
+      <form>
+        <h4 className="title">LOGIN FORM</h4>
+
+        <div className="mb-3">
+          <input
+            type="email"
+            autoFocus
+            name="email"
+            value={loginData.email}
+            onChange={handleChange}
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Email "
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            name="password"
+            value={loginData.password}
+            onChange={handleChange}
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={validations}
+          disabled={isSubmitting || disabled}
+        >
+          {isSubmitting ? (
+            <>
+              <span
+                className="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Submitting...</span>
+            </>
+          ) : (
+            "LOGIN"
+          )}
+        </button>
+        <Toaster />
+        <br />
+        <span className="">Don't have an account?</span>
+        <Link to="/register">Signup</Link>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,9 +1,230 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+
+import "./Auth.css";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-export default Register
+  const navigate = useNavigate();
+
+  const validateAndSetError = (value, validationFunction, errorSetter) => {
+    if (!validationFunction(value)) {
+      errorSetter();
+      return false;
+    } else {
+      setFormState((prevState) => ({
+        ...prevState,
+        [errorSetter.name]: "",
+      }));
+      return true;
+    }
+  };
+
+  const validateFirstName = (firstName) => {
+    return firstName.trim().length !== 0;
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/;
+    return regex.test(password);
+  };
+
+  const displayErrorToast = (error) => {
+    toast.error(error);
+  };
+
+  const handleInvalidFirstName = () => {
+    const errorMessage = "Please enter a valid first name";
+    setFormState((prevState) => ({
+      ...prevState,
+      firstNameError: errorMessage,
+    }));
+    displayErrorToast(errorMessage);
+  };
+
+  const handleInvalidEmail = () => {
+    const errorMessage = "Please enter a valid email address";
+    setFormState((prevState) => ({
+      ...prevState,
+      emailError: errorMessage,
+    }));
+    displayErrorToast(errorMessage);
+  };
+
+  const handlePasswordMismatch = () => {
+    setFormState((prevState) => ({
+      ...prevState,
+      confirmPasswordError: "Password do not match",
+    }));
+    displayErrorToast("Password do not match");
+  };
+
+  const handleInvalidPassword = () => {
+    const errorMessage =
+      "Password must be at least 6 characters, contain one capital letter, one digit, and one special character";
+    setFormState((prevState) => ({
+      ...prevState,
+      passwordError: errorMessage,
+    }));
+    displayErrorToast(errorMessage);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { password, confirmPassword, firstName, email } = formState;
+
+    if (
+      !validateAndSetError(firstName, validateFirstName, handleInvalidFirstName)
+    ) {
+      return;
+    }
+
+    if (!validateAndSetError(email, validateEmail, handleInvalidEmail)) {
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      handlePasswordMismatch();
+      return;
+    } else {
+      setFormState((prevState) => ({
+        ...prevState,
+        confirmPasswordError: "",
+      }));
+    }
+
+    if (
+      !validateAndSetError(password, validatePassword, handleInvalidPassword)
+    ) {
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/register`,
+        formState
+      );
+
+      if (res && res.data && res.data.message) {
+        toast.success("Registration successful. Please log in.");
+        navigate("/login");
+      } else {
+        toast.error(
+          res?.data?.message || "Registration failed. Please try again."
+        );
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+
+    setFormState((prevState) => ({
+      ...prevState,
+      passwordError: "",
+      confirmPasswordError: "",
+      firstNameError: "",
+      emailError: "",
+    }));
+  };
+
+  return (
+    <>
+      <div className="form-container">
+        <form>
+          <h4 className="title">REGISTER FORM</h4>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="firstName"
+              value={formState.firstName}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="First Name"
+              required
+              autoFocus
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="lastName"
+              value={formState.lastName}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Last Name"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="example@gmail.com"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formState.confirmPassword}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            REGISTER
+          </button>
+          <Toaster />
+          <br />
+          <span className="">Already have an account?</span>
+          <Link to="/login">Login</Link>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
